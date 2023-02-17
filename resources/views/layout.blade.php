01fd8b2f95cbd2d7b56ca0632bff7015ea1acb28
Joe Tannenbaum <joe@joe.codes>
2023-02-17 09:59:30 -0500

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,8 @@
     <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml">
 
     <meta property="og:image" content="{{ url('/passkeys-blog-header.png') }}" />
+    <meta property="og:image:width" content="3000" />
+    <meta property="og:image:height" content="2000" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="{{ url('/') }}" />
     <meta property="og:title" content="Laravel Passkey Authentication Demo | Joe Codes" />
